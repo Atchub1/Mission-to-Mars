ddcdfb6533a6f2736daaf270c190697deda77c61
Atchub1 <amytcleveland@gmail.com>
2019-10-29 11:02:42 -0500

    update scrape function

--- Mission_to_Mar_webpage/app.py
+++ Mission_to_Mar_webpage/app.py
@@ -1,17 +1,11 @@
-# import libraries
+# Import libraries
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
 
-# create instance of Flask app
+# Create instance of Flask app
 app = Flask(__name__)
-
-# # Create connection variable
-# conn = 'mongodb://localhost:27017'
-
-# # Pass connection to the pymongo instance.
-# client = pymongo.MongoClient(conn)
 
 
 # Use flask_pymongo to set up mongo connection
@@ -19,10 +13,8 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
     
 
-
-# create route to query Mongo and pass the mars data into the HTML template to display.
+# Create route to query Mongo and pass the mars data into the HTML template to display.
 @app.route("/")
-
 def index():
 
     mars_web = mongo.db.mars.find_one()
@@ -31,28 +23,13 @@ def index():
 
 
 
-# create route that renders mission to mars template
+# Create route that renders mission to mars template
 @app.route("/scrape")
 def scrapper():
     mars_web = mongo.db.mars
     mars_data = scrape_mars.scrape()
     mars_web.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
-
-
-#         render_template("scrape_mars.py")
-
-#     # Connect to/create the mars database
-#     db = mongo.mars_db
-
-#     # Drop the collection, if available, to remove duplicates
-#     db.mars.drop()
-
-#     # create the collection and insert the scrap results as one document
-#     db.mars.insert_one(
-#         mars_web_dict
-#         )
-
 
 
 
